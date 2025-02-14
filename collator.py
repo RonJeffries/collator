970dd3cf83bbd8b3ac6f7b60159d40662407fc0f
Ron Jeffries <ronjeffries@acm.org>
2025-02-14 08:09:57 -0500

    refactor `aged_names` for simplicity and clarity.

--- collator.py
+++ collator.py
@@ -23,12 +23,18 @@ class Collator:
             self.new_names.append(name)
 
     def aged_names(self):
+        yield from self.yield_known_names()
+        yield from self.yield_and_age_new_names()
+        self.new_names = []
+
+    def yield_known_names(self):
         for name in self.known_names:
             yield AgedName(name=name, is_new=False)
+
+    def yield_and_age_new_names(self):
         for name in self.new_names:
             self.known_names.append(name)
             yield AgedName(name=name, is_new=True)
-        self.new_names = []
 
     def outcome_for(self, name:str):
         try:
