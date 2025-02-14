fb9f3deb66c6f454d51eeda541b675ac02055693
Ron Jeffries <ronjeffries@acm.org>
2025-02-14 09:29:38 -0500

    extracting Sequencer

--- collator.py
+++ collator.py
@@ -45,18 +45,7 @@ class Collator:
         self.sequencer.add_name(name)
 
     def aged_names(self):
-        yield from self.yield_known_names()
-        yield from self.yield_and_age_new_names()
-        self.new_names = []
-
-    def yield_known_names(self):
-        for name in self.known_names:
-            yield AgedName(name=name, is_new=False)
-
-    def yield_and_age_new_names(self):
-        for name in self.new_names:
-            self.known_names.append(name)
-            yield AgedName(name=name, is_new=True)
+        yield from self.sequencer.aged_names()
 
     def outcome_for(self, name:str):
         try:
--- sequencer.py
+++ sequencer.py
@@ -1,3 +1,5 @@
+from aged_name import AgedName
+
 
 class Sequencer:
     def __init__(self):
@@ -11,3 +13,17 @@ class Sequencer:
     def add_name(self, name:str):
         if name not in self.known_names and name not in self.new_names:
             self.new_names.append(name)
+
+    def aged_names(self):
+        yield from self.yield_known_names()
+        yield from self.yield_and_age_new_names()
+        self.new_names = []
+
+    def yield_known_names(self):
+        for name in self.known_names:
+            yield AgedName(name=name, is_new=False)
+
+    def yield_and_age_new_names(self):
+        for name in self.new_names:
+            self.known_names.append(name)
+            yield AgedName(name=name, is_new=True)
