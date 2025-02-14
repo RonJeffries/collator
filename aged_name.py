f00054da1ea12a6cd4660c4bd3cbf5eefb26d604
Ron Jeffries <ronjeffries@acm.org>
2025-02-14 08:18:40 -0500

    convert AgedName to dataclass.

--- aged_name.py
+++ aged_name.py
@@ -1,5 +1,6 @@
+from dataclasses import dataclass
 
+@dataclass
 class AgedName:
-    def __init__(self, name:str, is_new:bool):
-        self.name = name
-        self.is_new = is_new
+    name: str
+    is_new: bool
