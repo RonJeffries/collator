ab05a8b8ebf994411fe25c6b40cf654a63cac6ce
Ron Jeffries <ronjeffries@acm.org>
2025-02-15 11:42:46 -0500

    DictCollator working.

--- dict_collator.py
+++ dict_collator.py
@@ -1,7 +1,21 @@
+from result import Result
+
 
 class DictCollator:
+    def __init__(self):
+        self.collator = dict()
+        self.high_water = 0
+
     def __enter__(self):
+        self.high_water = len(self.collator)
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         pass
+
+    def add(self, name, outcome):
+        self.collator[name] = outcome
+
+    def results(self):
+        return [Result(name, outcome, i>=self.high_water)
+            for i, (name, outcome) in enumerate(self.collator.items())]
--- test_dict_collator.py
+++ test_dict_collator.py
@@ -2,6 +2,13 @@ from dict_collator import DictCollator
 
 
 class TestDictCollator:
+    @staticmethod
+    def check(results, index, name, outcome, new):
+        result = results[index]
+        assert result.name == name
+        assert result.outcome == outcome
+        assert result.is_new is new
+
     def test_exists(self):
         DictCollator()
 
@@ -9,3 +16,26 @@ class TestDictCollator:
         c = DictCollator()
         with c as d:
             pass
+
+    def test_add_two_and_report(self):
+        dc = DictCollator()
+        with dc as collator:
+            dc.add('TestFoo', "Pass")
+            dc.add('TestBar', "Fail")
+            results = dc.results()
+            assert len(results) == 2
+        self.check(results,
+           0, 'TestFoo', 'Pass', True)
+        self.check(results,
+           1, 'TestBar', 'Fail', True)
+
+    def test_old_and_new(self):
+        dc = DictCollator()
+        with dc as collator:
+            dc.add('TestFoo', "Fail")
+        with dc as collator:
+            dc.add('TestBar', "Pass")
+            dc.add('TestFoo', "Fail")
+            results = dc.results()
+        self.check(results,0, 'TestFoo', 'Fail', False)
+        self.check(results,1, 'TestBar', 'Pass', True)
